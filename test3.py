90f9f8956bf796381c76546025d3824a5f750f49
Sam <sam101567@gmail.com>
2023-12-03 19:20:29 +0800

    Cleaned up the code

--- test3.py
+++ test3.py
@@ -9,11 +9,8 @@ class FittingRoom:
         self.green_count = 0
         self.lock = threading.Lock()
         self.lock2 = threading.Lock()
-        self.lock3 = threading.Lock()
         self.blue_semaphore = threading.Semaphore(n)
         self.green_semaphore = threading.Semaphore(n)
-        self.blue_turn = threading.Event()
-        self.green_turn = threading.Event()
         self.blue_turn_semaphore = threading.Semaphore(0)
         self.green_turn_semaphore = threading.Semaphore(0)
 
@@ -23,20 +20,17 @@ class FittingRoom:
             turn_semaphore = self.blue_turn_semaphore
             self.blue_count += 1
             other_count = self.green_count
-            turn_event = self.blue_turn
         elif color == "green":
             semaphore = self.green_semaphore
             turn_semaphore = self.green_turn_semaphore
             self.green_count += 1
             other_count = self.blue_count
-            turn_event = self.green_turn
             
 
         with self.lock:
             if other_count > 0 or self.slots == 0:
                 print(f"{color.capitalize()} thread {thread_id} is in the waiting room.")
                 self.lock.release()
-                turn_event.wait()  # Wait for the other color to finish entering
                 turn_semaphore.acquire()
                 self.lock.acquire()
                 
@@ -54,17 +48,13 @@ class FittingRoom:
             semaphore = self.blue_semaphore
             self.blue_count -= 1
             other_count = self.green_count
-            other_turn = self.green_turn
             other_semaphore = self.green_turn_semaphore
-            own_turn = self.blue_turn
             own_semaphore = self.blue_turn_semaphore
         elif color == "green":
             semaphore = self.green_semaphore
             self.green_count -= 1
             other_count = self.blue_count
-            other_turn = self.blue_turn
             other_semaphore = self.blue_turn_semaphore
-            own_turn = self.green_turn
             own_semaphore = self.green_turn_semaphore
         with self.lock2:
             semaphore.release()
@@ -74,10 +64,8 @@ class FittingRoom:
             if self.slots == self.n:
                 print("Empty fitting room. ")
                 if(other_count != 0):
-                    other_turn.set()  # Signal the other color to enter
                     other_semaphore.release(n)
                 else:
-                    own_turn.set()
                     own_semaphore.release(n)
 
 
